cfe63c946e1ad0db933cfaaba402f10c35e46f0e
Matt Riley <matthew.riley@barren.kyschools.us>
2024-11-20 08:01:14 -0600

    Initial Commit

---
+++ TriviaExample/TriviaExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaExampleApp.swift
+//  TriviaExample
+//
+//  Created by BCCS 2022 on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
