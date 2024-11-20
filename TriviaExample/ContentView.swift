e40c8673b565b196dffee00ba363410d3d8426fa
Matt Riley <matthew.riley@barren.kyschools.us>
2024-11-20 08:09:35 -0600

    Created the questions variable which is an array of dictionaries

--- TriviaExample/ContentView.swift
+++ TriviaExample/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var questions = [
+        [
+            "question": "What is 1 + 1?",
+            "answers" : [
+                1,
+                2,
+                3,
+                4
+            ],
+            "correctAnswer": 2
+        ],
+        [
+            "question": "What is 2 - 2?",
+            "answers" : [
+                0,
+                2,
+                -2,
+                4
+            ],
+            "correctAnswer": 0
+        ],
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("TRIVIA GAME")
         }
-        .padding()
     }
 }
 
