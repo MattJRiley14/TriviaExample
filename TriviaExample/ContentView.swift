337cee4cc90bf502b8b2479ee4162b1e1e45e17f
Matt Riley <matthew.riley@barren.kyschools.us>
2024-11-22 08:49:20 -0600

    Changed the storing of question and answers to an array with only string values. Also, created next question button and next question function.

--- TriviaExample/ContentView.swift
+++ TriviaExample/ContentView.swift
@@ -9,35 +9,92 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var questions = [
-        [
-            "question": "What is 1 + 1?",
-            "answers" : [
-                1,
-                2,
-                3,
-                4
-            ],
-            "correctAnswer": 2
-        ],
-        [
-            "question": "What is 2 - 2?",
-            "answers" : [
-                0,
-                2,
-                -2,
-                4
-            ],
-            "correctAnswer": 0
-        ]
+    //    var questions = [
+    //        [
+    //            "question": "What is 1 + 1?",
+    //            "answers" : [
+    //                1,
+    //                2,
+    //                3,
+    //                4
+    //            ],
+    //            "correctAnswer": 2
+    //        ],
+    //        [
+    //            "question": "What is 2 - 2?",
+    //            "answers" : [
+    //                0,
+    //                2,
+    //                -2,
+    //                4
+    //            ],
+    //            "correctAnswer": 0
+    //        ]
+    //    ]
+        
+    let questions = [
+        "What is 1 + 1?",
+        "What is 1 - 1?"
     ]
     
+    let answers = [
+        [
+            ["1", "Wrong"],
+            ["2", "Correct"],
+            ["3", "Wrong"],
+            ["4", "Wrong"]
+        ],
+        [
+            ["-2", "Wrong"],
+            ["-1", "Wrong"],
+            ["0", "Correct"],
+            ["2", "Wrong"]
+        ]
+    ]
+
     @State private var questionIndex = 0
     
     var body: some View {
         VStack {
             Text("TRIVIA GAME")
+
+            Text(questions[questionIndex])
+
+            Button {
+                print("Hello")
+            } label: {
+                Text(answers[questionIndex][0][0])
+            }
+
+            Button {
+                print("Hello")
+            } label: {
+                Text(answers[questionIndex][1][0])
+            }
+
+            Button {
+                print("Hello")
+            } label: {
+                Text(answers[questionIndex][2][0])
+            }
+
+            Button {
+                print("Hello")
+            } label: {
+                Text(answers[questionIndex][3][0])
+            }
+
+            Button {
+                nextQuestion()
+            } label: {
+                Text("Next Question")
+            }
+
         }
+    }
+    
+    func nextQuestion() {
+        questionIndex += 1
     }
 }
 
